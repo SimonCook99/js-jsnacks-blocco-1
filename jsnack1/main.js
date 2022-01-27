2919fb10a31d490bffbbd6aeb96476a123036322
Simone Mauro <simon.mauro@virgilio.it>
2022-01-27 16:04:17 +0100

    Aggiornamento controlli

--- jsnack1/main.js
+++ jsnack1/main.js
@@ -2,15 +2,16 @@ let numeriUtente = [];
 
 let titoloSomma = document.getElementById("somma");
 
+//variabile della somma degli elementi
 let sommaTotale = 0;
 
 for(let i = 0; i < 10; i++){
     let numeroCorrente = parseInt(prompt("Inserisci il " + (i+1) + "° numero del vettore"));
 
+    //se il numero inserito non è valido, lo chedo nuovamente, e solo quando è valido procedo al push nel vettore
     while(numeroCorrente < 0 || isNaN(numeroCorrente)){
         console.log(numeriUtente);
-        let numeroCorrente = parseInt(prompt("Valore non valido, inserisci il " + (i+1) + "° numero del vettore (numero > 0)"));
-        numeriUtente[i] = numeroCorrente;
+        numeroCorrente = parseInt(prompt("Valore non valido, inserisci il " + (i+1) + "° numero del vettore (numero > 0)"));
     }
 
     numeriUtente.push(numeroCorrente);
