74ccbb599bd350f6abd7c771c6a8861b22e44b82
Simone Mauro <simon.mauro@virgilio.it>
2022-01-27 16:56:09 +0100

    aggiornamento

--- jsnack3/main.js
+++ jsnack3/main.js
@@ -1,4 +1,4 @@
-let numeri = [1,2,3,4,5,6,7,8,9,10];
+let numeri = [1,2,3,4,5,6,7,8,9,10]; //vettore con i primi 10 numeri
 
 let titoloSommaMedia = document.getElementById("somma-media");
 
@@ -9,7 +9,7 @@ for(let i = 0; i < numeri.length; i++){
     sommaTotale += numeri[i];
 }
 
-mediaTotale = sommaTotale / numeri.length
+mediaTotale = sommaTotale / numeri.length;
 
 
-titoloSommaMedia.innerHTML = "La somma dei primi 10 numeri inseriti è: " + sommaTotale + " mentre la media è: " + mediaTotale;
+titoloSommaMedia.innerHTML = "La somma dei primi 10 numeri è: " + sommaTotale + " mentre la media è: " + mediaTotale;
